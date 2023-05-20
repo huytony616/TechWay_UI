220843ea2c7412fd361098d3a81c5a5ad646acd0
huytony616 <huytony616@gmail.com>
2023-05-21 02:52:14 +0700

    menu pointer

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
-  constructor(private router: Router){}
+export class MenuComponent implements OnInit {
+  constructor(private router: Router, public authSVC: UserService) {}
+
+  img: string = '';
   logout() {
     localStorage.removeItem('jwtToken');
     window.location.reload();
+  }
+
+  ngOnInit(): void {
+    this.img = this.authSVC.decodedToken.photo.split('>')[0];
+    console.log(this.authSVC.decodedToken);
   }
 }
